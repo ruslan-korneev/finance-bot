324f20cc89bbe76af0e8b9186117aa7ad28a5c66
shaggy-axel <kornerus@fjfi.cvut.cz>
2021-08-03 22:12:43 +0300

    Add run scripts

---
+++ scripts/run_bot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VENV=./venv
+DEPLOY_FLAG=/opt/finance/deploy_state.flag
+
+touch $DEPLOY_FLAG
+
+if [ ! -d $VENV ]; then
+    `which python3` -m venv $VENV
+    $VENV/bin/pip install -U pip
+fi
+
+$VENV/bin/pip install -U pip
+echo "$?"
+
+$VENV/bin/pip install -r requirements.txt
+echo "$?"
+
+$VENV/bin/python src/bot/bot.py
+
+rm -f $DEPLOY_FLAG
+
+echo "Run Bot"
