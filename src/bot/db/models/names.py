5a5b1cb31f60ed282f761147de7b581004f57c70
shaggy-axel <kornerus@fjfi.cvut.cz>
2021-08-05 09:55:34 +0300

    Add name-models for using bot

--- src/bot/db/models/names.py
+++ src/bot/db/models/names.py
@@ -12,3 +12,15 @@ class NameExpense(Base):
     __tablename__ = 'expense_names'
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
+
+
+class NameAsset(Base):
+    __tablename__ = 'asset_names'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+
+
+class NameLiability(Base):
+    __tablename__ = 'liability_names'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
